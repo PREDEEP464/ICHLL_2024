fdc4a6f51553231d516f3baac01c3b013a6bb25c
Predeep <predeepkumar.us2022cse@sece.ac.in>
2024-04-04 09:01:44 +0530

    Schedule Table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 import './Table.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const Table = () => {
   const scheduleData = [
@@ -15,11 +13,11 @@ const Table = () => {
   return (
     <div className="row z-1 relative flex justify-center bg-blue-900 p-5 h-96 w-full items-center rounded-md">
       <div className='col-lg-6'>
-      <h4 className=' bg-warning text-white text-center p-1 mb-0 heading font-bold'>Important Dates</h4>
-        <table className=' main mb-3 w-auto size-72'>
+        <h4 className=' bg-warning text-white text-center p-1 mb-0 heading font-bold'>Important Dates</h4>
+        <table className='main mb-3 w-auto size-72'>
           <tbody>
             {scheduleData.map((item, index) => (
-              <tr className='tabl'key={index}>
+              <tr className='tabl' key={index}>
                 <td className='f-c text-white font-bold'>{item.event}</td>
                 <td className='s-c text-yellow-400 font-bold'>{item.date}</td>
                 <td className='t-c text-yellow-400 font-bold'>{item.status || 'Open'}</td>
@@ -28,29 +26,24 @@ const Table = () => {
           </tbody>
         </table>
       </div>
-      
-      
       <div className='photos col-lg-6 mr-2 font-sans'>
-            <div className='row gap-6 flex justify-center '>
-              <div className='hover:cursor-pointer text-center w-auto p-5 bg-primary rounded-lg col-lg-2 col-md-4 col-sm-12'>
-                <img className="bg-red-500 p-5 rounded-lg size-40 hover:scale-125" src="https://icaccs.sece.ac.in/images/all-icon/ctg-1.png" alt="" />
-                <p className='font-bold text-white overflow-clip mt-3 pt-3'>Submission</p>
-              </div>
-              <div className='hover:cursor-pointer text-center w-auto p-5 bg-success rounded-lg col-lg-2 col-md-4 col-sm-12'>
-                <img className="bg-blue-500 p-5 rounded-lg size-40 hover:scale-125  " src="https://icaccs.sece.ac.in/images/all-icon/ctg-2.png" alt="" />
-                <p className='font-bold text-white text-clip mt-3 pt-3'>Review System</p>
-              </div>
-              <div className='hover:cursor-pointer text-center w-auto p-5 bg-danger rounded-lg col-lg-2 col-md-4 col-sm-12'>
-                <img className="bg-green-700 p-5 rounded-lg size-40 hover:scale-125" src="https://icaccs.sece.ac.in/images/all-icon/ctg-3.png" alt="" />
-                <p className='font-bold text-white overflow-clip mt-3 pt-3'>Camera Ready</p>
-              </div>
-            </div>
-            
+        <div className='row gap-6 flex justify-center '>
+          <div className='hover:cursor-pointer text-center w-auto p-5 bg-primary rounded-lg col-lg-2 col-md-4 col-sm-12'>
+            <img className="bg-red-500 p-5 rounded-lg size-40 hover:scale-125" src="https://icaccs.sece.ac.in/images/all-icon/ctg-1.png" alt="" />
+            <p className='font-bold text-white overflow-clip mt-3 pt-3'>Submission</p>
+          </div>
+          <div className='hover:cursor-pointer text-center w-auto p-5 bg-success rounded-lg col-lg-2 col-md-4 col-sm-12'>
+            <img className="bg-blue-500 p-5 rounded-lg size-40 hover:scale-125  " src="https://icaccs.sece.ac.in/images/all-icon/ctg-2.png" alt="" />
+            <p className='font-bold text-white text-clip mt-3 pt-3'>Review System</p>
+          </div>
+          <div className='hover:cursor-pointer text-center w-auto p-5 bg-danger rounded-lg col-lg-2 col-md-4 col-sm-12'>
+            <img className="bg-green-700 p-5 rounded-lg size-40 hover:scale-125" src="https://icaccs.sece.ac.in/images/all-icon/ctg-3.png" alt="" />
+            <p className='font-bold text-white overflow-clip mt-3 pt-3'>Camera Ready</p>
+          </div>
+        </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Table
-
+export default Table;
